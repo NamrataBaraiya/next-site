b02537cce2076c72b3eb3ec648ca3676217bfbea
khanshahnawaz8080 <khanshahnawaz8080@gmail.com>
2025-09-25 14:04:59 +0530

    Hero & CTA updated

--- components/hero-06/hero-06.tsx
+++ components/hero-06/hero-06.tsx
@@ -20,23 +20,23 @@ const Hero06 = () => {
           </Link>
         </Badge>
         <h1 className="mt-6 text-4xl sm:text-5xl md:text-6xl lg:text-7xl md:leading-[1.2] font-semibold tracking-tighter">
-          Customized Shadcn UI Blocks & Components
+          Send Cold Emails to inbox<br></br>
+            without Gsuite Accounts.
         </h1>
         <p className="mt-6 md:text-lg">
-          Explore a collection of Shadcn UI blocks and components, ready to
-          preview and copy. Streamline your development workflow with
-          easy-to-implement examples.
+          Fully Managed Email Infrastructure to send<br></br>
+          upto 15k emails per domain per month.
         </p>
         <div className="mt-12 flex items-center justify-center gap-4">
           <Button size="lg" className="rounded-full text-base">
-            Get Started <ArrowUpRight className="h-5! w-5!" />
+            Start for Free<ArrowUpRight className="h-5! w-5!" />
           </Button>
           <Button
             variant="outline"
             size="lg"
             className="rounded-full text-base shadow-none"
           >
-            <CirclePlay className="h-5! w-5!" /> Watch Demo
+            Get Demo & Product Tour <CirclePlay className="h-5! w-5!" />
           </Button>
         </div>
       </div>
